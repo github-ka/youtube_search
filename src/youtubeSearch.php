9c5b1968d35317e95e00fdac6e725952a8b510ce
兼城賢人 <>
2020-05-15 23:13:44 +0900

    簡易検索フォーム 追加

--- src/youtubeSearch.php
+++ src/youtubeSearch.php
@@ -4,75 +4,91 @@ require_once __DIR__ . '/../lib/vendor/autoload.php';
 
 define("API_KEY", "AIzaSyBcQHyxgAiHlu7qpSzGtbHoin-yWDGBj1A");
 
-//認証
-$client = new Google_Client();
-$client->setApplicationName("My Application");
-$client->setDeveloperKey(API_KEY);
 
-$youtube = new Google_Service_YouTube($client);
+if (!empty($_GET['keyword'])) {
+  $key = isset($_GET['keyword']) ? $_GET['keyword'] : null;
+  //認証
+  $client = new Google_Client();
+  $client->setApplicationName("My Application");
+  $client->setDeveloperKey(API_KEY);
 
-
-$keyword = "マコなり";
-$params['q'] = $keyword;
-$params['type'] = 'video';
-$params['maxResults'] = 2;
+  $youtube = new Google_Service_YouTube($client);
 
 
-$videos = [];
-try {
 
-  $searchRes = $youtube->search->listSearch('snippet', $params);
-
-  array_map(function ($searchRes) use (&$videos) {
-      $videos[] = $searchRes;
-    }, $searchRes['items']
-  );
-
-  // $i = 0;
-  // foreach($searchRes['items'] as $v){
-  //   $videos[$i] = $v[$i];
-  //   $i++;
-  // }
-
-  // echo '<pre>';
-  // var_dump($videos);
-  // echo '</pre>';
-  // exit;
+  $keyword = $key;
+  $params['q'] = $keyword;
+  $params['type'] = 'video';
+  $params['maxResults'] = 10;
 
 
-} catch (Google_Service_Exception $e) {
-  echo htmlspecialchars($e->getMessage());
-  exit;
-} catch (Google_Exception $e) {
-  echo htmlspecialchars($e->getMessage());
-  exit;
+  $videos = [];
+
+  try {
+    $searchRes = $youtube->search->listSearch('snippet', $params);
+
+    // $ss = function ($sr) use (&$videos) {
+    //   return $videos[] = $sr;
+    // };
+    // array_map($ss, $searchRes['items']);
+
+    array_map(
+      function ($searchRes) use (&$videos) {
+        $videos[] = $searchRes;
+      },
+      $searchRes['items']
+    );
+  } catch (Google_Service_Exception $e) {
+    echo htmlspecialchars($e->getMessage());
+    exit;
+  } catch (Google_Exception $e) {
+    echo htmlspecialchars($e->getMessage());
+    exit;
+  }
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
+  <style type="text/css">
+    .mg {
+      margin: 20px 30px;
+    }
+  </style>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>サーチ</title>
 </head>
 
 <body>
-  <table>
-    <?php
-    if ($videos) {
-      foreach ($videos as $v) {
-        echo '<tr>';
-        echo '<td><img src="'. $v['snippet']['thumbnails']['default']['url'] .'" /></td>';
-        // echo '<td>title</td>';
-        echo '<td>' . $v['snippet']['title'] . '</td>';
-        echo '</tr>';
-      }
+
+  <div class="mg">
+    <form action="" method="get">
+      <input type="text" name="keyword">
+      <input type="submit" value="検索">
+    </form>
+  </div>
+
+
+
+  <?php
+  if (!empty($videos)) {
+    echo '<table>';
+    foreach ($videos as $v) {
+      echo '<tr>';
+      echo '<td><img src="' . $v['snippet']['thumbnails']['default']['url'] . '" /></td>';
+      // echo '<td>title</td>';
+      echo '<td>' . $v['snippet']['title'] . '</td>';
+      echo '</tr>';
     }
-    ?>
+    echo '</table>';
+  }
+  ?>
 
 
-  </table>
 
 </body>
 
